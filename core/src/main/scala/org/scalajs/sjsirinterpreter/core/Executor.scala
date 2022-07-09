3b704bdfc2475b1eeb0adbae45c704b5d314a8d4
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-07-09 13:35:01 +0200

    Move Stack to its own file.

--- core/src/main/scala/org/scalajs/sjsirinterpreter/core/Executor.scala
+++ core/src/main/scala/org/scalajs/sjsirinterpreter/core/Executor.scala
@@ -21,54 +21,6 @@ import org.scalajs.sjsirinterpreter.core.utils.Utils.OptionsOps
 
 import Types.TypeOps
 
-final class Stack {
-  import Stack._
-
-  val elements = js.Array[Element]()
-  var currentClassName: String = null
-  var currentMethodName: String = null
-
-  @inline
-  def enter[A](callSitePos: Position, className: ClassName, methodName: MethodName)(body: => A): A =
-    enter(callSitePos, className.nameString, methodName.simpleName.nameString)(body)
-
-  @inline
-  def enter[A](callSitePos: Position, className: ClassName, methodName: String)(body: => A): A =
-    enter(callSitePos, className.nameString, methodName)(body)
-
-  @inline
-  def enter[A](callSitePos: Position, className: String, methodName: String)(body: => A): A = {
-    val prevClassName = currentClassName
-    val prevMethodName = currentMethodName
-    elements.push(new Element(prevClassName, prevMethodName, callSitePos))
-    try {
-      currentClassName = className
-      currentMethodName = methodName
-      body
-    } finally {
-      currentClassName = prevClassName
-      currentMethodName = prevMethodName
-      elements.pop()
-    }
-  }
-
-  @inline
-  def enterJSCode[A](callSitePos: Position)(body: => A): A =
-    enter(callSitePos, "<jscode>", "<jscode>")(body)
-
-  def captureStackTrace(pos: Position): List[Element] = {
-    var result: List[Element] = Nil
-    for (i <- 0 until elements.length)
-      result ::= elements(i)
-    result ::= new Element(currentClassName, currentMethodName, pos)
-    result
-  }
-}
-
-object Stack {
-  final class Element(val className: String, val methodName: String, val pos: Position)
-}
-
 /**
   * Executor is an object performing the evaluation
   * and maintaining the global state of the program.
---
+++ core/src/main/scala/org/scalajs/sjsirinterpreter/core/Stack.scala
@@ -0,0 +1,54 @@
+package org.scalajs.sjsirinterpreter.core
+
+import scala.scalajs.js
+
+import org.scalajs.ir.Names._
+import org.scalajs.ir.Position
+
+final class Stack {
+  import Stack._
+
+  val elements = js.Array[Element]()
+  var currentClassName: String = null
+  var currentMethodName: String = null
+
+  @inline
+  def enter[A](callSitePos: Position, className: ClassName, methodName: MethodName)(body: => A): A =
+    enter(callSitePos, className.nameString, methodName.simpleName.nameString)(body)
+
+  @inline
+  def enter[A](callSitePos: Position, className: ClassName, methodName: String)(body: => A): A =
+    enter(callSitePos, className.nameString, methodName)(body)
+
+  @inline
+  def enter[A](callSitePos: Position, className: String, methodName: String)(body: => A): A = {
+    val prevClassName = currentClassName
+    val prevMethodName = currentMethodName
+    elements.push(new Element(prevClassName, prevMethodName, callSitePos))
+    try {
+      currentClassName = className
+      currentMethodName = methodName
+      body
+    } finally {
+      currentClassName = prevClassName
+      currentMethodName = prevMethodName
+      elements.pop()
+    }
+  }
+
+  @inline
+  def enterJSCode[A](callSitePos: Position)(body: => A): A =
+    enter(callSitePos, "<jscode>", "<jscode>")(body)
+
+  def captureStackTrace(pos: Position): List[Element] = {
+    var result: List[Element] = Nil
+    for (i <- 0 until elements.length)
+      result ::= elements(i)
+    result ::= new Element(currentClassName, currentMethodName, pos)
+    result
+  }
+}
+
+object Stack {
+  final class Element(val className: String, val methodName: String, val pos: Position)
+}
