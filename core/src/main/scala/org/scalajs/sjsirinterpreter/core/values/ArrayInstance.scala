22cba58dc26970ab1e9c7302d8f03f81321bd534
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-07-13 11:02:53 +0200

    Create the contents array of ArrayInstance outside its constructor.

--- core/src/main/scala/org/scalajs/sjsirinterpreter/core/values/ArrayInstance.scala
+++ core/src/main/scala/org/scalajs/sjsirinterpreter/core/values/ArrayInstance.scala
@@ -11,26 +11,16 @@ import org.scalajs.ir.Types._
 
 import org.scalajs.sjsirinterpreter.core.Types
 
-class ArrayInstance private (private[core] final val typeRef: ArrayTypeRef, length: Int) extends js.Object {
-  private[core] final val contents: Array[js.Any] = {
-    val a = new Array[js.Any](length)
-    val zero = if (typeRef.dimensions > 1) null else Types.zeroOfRef(typeRef.base)
-    for (i <- 0 until length)
-      a(i) = zero
-    a
-  }
-
+class ArrayInstance private (
+  private[core] final val typeRef: ArrayTypeRef,
+  private[core] final val contents: Array[js.Any],
+) extends js.Object {
   override def toString(): String = s"${typeRef.displayName}@${hashCode().toHexString}"
 }
 
 object ArrayInstance {
-  def fromList(typeRef: ArrayTypeRef, list: List[js.Any]): ArrayInstance = {
-    val instance = new ArrayInstance(typeRef, list.size)
-    list.zipWithIndex.foreach {
-      case (element, i) => instance.contents(i) = element
-    }
-    instance
-  }
+  def fromList(typeRef: ArrayTypeRef, list: List[js.Any]): ArrayInstance =
+    new ArrayInstance(typeRef, list.toArray)
 
   def createWithDimensions(typeRef: ArrayTypeRef, lengths: List[Int]): ArrayInstance = {
     if (lengths.isEmpty || lengths.sizeIs > typeRef.dimensions)
@@ -39,20 +29,19 @@ object ArrayInstance {
     val length = lengths.head
     val tailLengths = lengths.tail
 
-    val result = new ArrayInstance(typeRef, length)
-    if (tailLengths.nonEmpty) {
+    val resultContents = new Array[js.Any](length)
+    if (tailLengths.isEmpty) {
+      val zero = if (typeRef.dimensions > 1) null else Types.zeroOfRef(typeRef.base)
+      java.util.Arrays.fill(resultContents.asInstanceOf[Array[AnyRef]], zero)
+    } else {
       val innerTypeRef = ArrayTypeRef(typeRef.base, typeRef.dimensions - 1)
       for (i <- 0 until length)
-        result.contents(i) = createWithDimensions(innerTypeRef, tailLengths)
+        resultContents(i) = createWithDimensions(innerTypeRef, tailLengths)
     }
-    result
+
+    new ArrayInstance(typeRef, resultContents)
   }
 
-  def clone(other: ArrayInstance): ArrayInstance = {
-    val otherContents = other.contents
-    val length = otherContents.length
-    val result = new ArrayInstance(other.typeRef, length)
-    System.arraycopy(otherContents, 0, result.contents, 0, length)
-    result
-  }
+  def clone(other: ArrayInstance): ArrayInstance =
+    new ArrayInstance(other.typeRef, other.contents.clone())
 }
